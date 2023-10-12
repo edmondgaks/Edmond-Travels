304a04404471dee14f2d974ff2b02dbe6926aa74
edmondgaks <edmondgaks@gmail.com>
2023-10-12 12:14:59 +0200

    starting page: LANDING ROUTES

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -6,7 +6,7 @@ const Landing = () => {
     return (
         <div className="p-3">
             <div className="bg-[url('/landing-image.jpeg')] w-full bg-fixed bg-cover rounded-md h-[96vh] box-border">
-                <div className="container-gradient rounded-md">
+                <div className="container-gradient rounded-md relative">
                     <div className="mx-auto flex flex-row items-center  justify-around py-5">
                         <Logo />
                         <Navbar />
@@ -16,6 +16,7 @@ const Landing = () => {
                         <div className="font-semibold text-4xl">Don't call it a dream. Call it a plan</div>
                         <div className="font-medium text-md opacity-90">Travel the way you wish to go with Phnes Travels helping you discover, live and travel at your own pace.</div>
                     </div>
+                    <div className="bg-white p-4 w-[50%] absolute inset-x-0 bottom-40 mx-auto"></div>
                 </div>
             </div>
         </div>
