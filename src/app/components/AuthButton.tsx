27dfe8bd5b01bbf76fca99f6e3636fb3dede18ef
edmondgaks <edmondgaks@gmail.com>
2023-10-15 10:37:00 +0200

    adding the user profile dropdown

--- src/app/components/AuthButton.tsx
+++ src/app/components/AuthButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { Heart } from "lucide-react"
+import { ChevronRight, Cog, CreditCard, Heart, LocateFixed, LogOut, MoveLeft, MoveRight, Settings, User, User2, UserCircle } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { profile } from "console";
@@ -19,13 +19,61 @@ export const UserProfile = () => {
             <div className="flex flex-row items-center gap-3" onClick={() => {setProfile(true)}}>
                 <div className="flex flex-row items-center gap-2 border-r-[2px] p-1 border-gray-400">
                     <Heart fill="#051721" size={18} />
-                    <div className="text-[#051721] text-xs font-semibold">Favourites</div>
+                    <div className="text-[#051721] text-xs font-medium">Favourites</div>
                 </div>
                 <Image src={"/annapie.png"} width={30} height={30} alt="User pic" />
-                <p className="text-xs text-[#051721] font-semibold">Ann pie</p>
+                <p className="text-xs text-[#051721] font-medium">Ann pie</p>
             </div>
             {profile && (
-                <div className="absolute top-14 w-[30vh] h-[10vh] bg-white shadow-md "></div>
+                <div className="absolute top-14 w-[35vh] px-5 py-3 flex rounded-lg flex-col gap-4 z-10 h-[40vh] bg-white shadow-md">
+                    <div className="flex flex-row gap-2 items-center">
+                        <Image src={"/annapie.png"} width={30} height={30} alt="User pic" />
+                        <div className="flex flex-col justify-center">
+                            <h1 className="font-medium text-[#051721] text-sm">Ann Pie</h1>
+                            <p className="text-gray-400 text-xs">Online</p>
+                        </div>
+                    </div>
+                    <div className="w-full bg-[#A9C3C3] h-[0.5px]"></div>
+                    <div className="flex flex-col justify-center gap-3">
+                        <div className="flex flex-row justify-between items-center">
+                            <div className="flex flex-row gap-2 items-center">
+                                <UserCircle size={18} />
+                                <h1 className="text-xs font-medium text-[#37454D]">My account</h1>
+                            </div>
+                            <ChevronRight size={20} />
+                        </div>
+                        <div className="flex flex-row justify-between items-center">
+                            <div className="flex flex-row gap-2 items-center">
+                                <CreditCard size={18} />
+                                <h1 className="text-xs font-medium text-[#37454D]">Payment</h1>
+                            </div>
+                            <ChevronRight size={20} />
+                        </div>
+                        <div className="flex flex-row justify-between items-center">
+                            <div className="flex flex-row gap-2 items-center">
+                                <Cog size={18} />
+                                <h1 className="text-xs font-medium text-[#37454D]">Settings</h1>
+                            </div>
+                            <ChevronRight size={20} />
+                        </div>
+                    </div>
+                    <div className="w-full bg-[#A9C3C3] h-[0.5px]"></div>
+                    <div className="flex flex-col justify-center gap-3">
+                        <div className="flex flex-row justify-between items-center">
+                            <div className="flex flex-row gap-2 items-center">
+                                <LocateFixed size={18} />
+                                <h1 className="text-xs font-medium text-[#37454D]">Support</h1>
+                            </div>
+                            <ChevronRight size={20} />
+                        </div>
+                        <div className="flex flex-row justify-between items-center">
+                            <div className="flex flex-row gap-2 items-center">
+                                <LogOut size={18} />
+                                <h1 className="text-xs font-medium text-[#37454D]">Logout</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             )}
         </div>
     )
